f06de21e3a04922ff29903dabafb6002480febdf
kbell <kbell140@gmail.com>
2019-04-05 14:04:06 -0400

    clean

---
+++ NBAMvc1.1/Controllers/MyTeamsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NBAMvc1._1.Data;
+using NBAMvc1._1.Models;
+
+namespace NBAMvc1._1.Controllers
+{
+    [Authorize]
+    public class MyTeamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MyTeamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MyTeams
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.MyTeam.Include(m => m.UserNav);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: MyTeams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var myTeam = await _context.MyTeam
+                .Include(m => m.UserNav)
+                .FirstOrDefaultAsync(m => m.MyTeamID == id);
+            if (myTeam == null)
+            {
+                return NotFound();
+            }
+
+            return View(myTeam);
+        }
+
+        // GET: MyTeams/Create
+        public IActionResult Create()
+        {
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: MyTeams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MyTeamID,UserID")] MyTeam myTeam)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(myTeam);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", myTeam.UserID);
+            return View(myTeam);
+        }
+
+        // GET: MyTeams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var myTeam = await _context.MyTeam.FindAsync(id);
+            if (myTeam == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", myTeam.UserID);
+            return View(myTeam);
+        }
+
+        // POST: MyTeams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MyTeamID,UserID")] MyTeam myTeam)
+        {
+            if (id != myTeam.MyTeamID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(myTeam);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MyTeamExists(myTeam.MyTeamID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", myTeam.UserID);
+            return View(myTeam);
+        }
+
+        // GET: MyTeams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var myTeam = await _context.MyTeam
+                .Include(m => m.UserNav)
+                .FirstOrDefaultAsync(m => m.MyTeamID == id);
+            if (myTeam == null)
+            {
+                return NotFound();
+            }
+
+            return View(myTeam);
+        }
+
+        // POST: MyTeams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var myTeam = await _context.MyTeam.FindAsync(id);
+            _context.MyTeam.Remove(myTeam);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MyTeamExists(int id)
+        {
+            return _context.MyTeam.Any(e => e.MyTeamID == id);
+        }
+    }
+}
--- NBAMvc1.1/Models/MyTeam.cs
+++ NBAMvc1.1/Models/MyTeam.cs
@@ -14,8 +14,67 @@ namespace NBAMvc1._1.Models
 
         [ForeignKey("UserNav")]
         public string UserID { get; set; }
-       
+
+
+        [ForeignKey("PG1Nav")]
+        public string PG1 { get; set; }
+
+        public virtual ApplicationUser PG1Nav { get; set; }
+        
+        [ForeignKey("PG2Nav")]
+        public string PG2 { get; set; }
+        
+        public virtual ApplicationUser PG2Nav { get; set; }
+
+
+
+        [ForeignKey("SG1Nav")]
+        public string SG1 { get; set; }
+
+        public virtual ApplicationUser SG1Nav { get; set; }
+
+        [ForeignKey("SG2Nav")]
+        public string SG2 { get; set; }
+
+        public virtual ApplicationUser SG2Nav { get; set; }
+
+
+
+        [ForeignKey("SF1Nav")]
+        public string SF1 { get; set; }
+
+        public virtual ApplicationUser SF1Nav { get; set; }
+
+        [ForeignKey("SF2Nav")]
+        public string SF2 { get; set; }
+
+        public virtual ApplicationUser SF2Nav { get; set; }
+
+
+
+        [ForeignKey("PF1Nav")]
+        public string PF1 { get; set; }
+
+        public virtual ApplicationUser PF1Nav { get; set; }
+
+        [ForeignKey("PF2Nav")]
+        public string PF2 { get; set; }
+
+        public virtual ApplicationUser PF2Nav { get; set; }
+
+
+
+        [ForeignKey("CNav")]
+        public string C { get; set; }
+
+        public virtual ApplicationUser CNav { get; set; }
+
+
+
+
         public virtual ApplicationUser UserNav { get; set; }
+
+
 
         public virtual ICollection<PlayerMyTeam> PlayerMyTeamNav { get; set; }
       
